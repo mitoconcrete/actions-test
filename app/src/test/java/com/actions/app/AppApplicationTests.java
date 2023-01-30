fb528241ccef25052fc866484d282890b9f0b753
mitoconcrete <xogns98@gmail.com>
2023-01-30 21:12:52 +0900

    fi

--- app/src/test/java/com/actions/app/AppApplicationTests.java
+++ app/src/test/java/com/actions/app/AppApplicationTests.java
@@ -1,5 +1,10 @@
 package com.actions.app;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +12,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 class AppApplicationTests {
 
   @Test
+  @DisplayName("테스트엔티티 테스트")
   void contextLoads() {
+      Car car = mock(Car.class);
+      when(car.getName()).thenReturn("자동차");
+      when(car.getSpeed()).thenReturn(10);
+
+      Car newCar = new Car();
+
+      assertEquals(car.getName(), newCar.getName());
+      assertEquals(car.getSpeed(), newCar.getSpeed());
   }
 
 }
